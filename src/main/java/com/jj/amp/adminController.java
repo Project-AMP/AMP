14f9eff146dd2dd6ae86f2ad2dc440dfd9420301
Tae-Hyeon-Lee <harry7141@gmail.com>
2022-05-30 02:27:40 +0900

    AMP

--- src/main/java/com/jj/amp/adminController.java
+++ src/main/java/com/jj/amp/adminController.java
@@ -12,9 +12,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Random;
-
 import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
